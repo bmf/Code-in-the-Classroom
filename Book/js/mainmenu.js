f9ff93db329db102931cf85eb190fc7cb486fdc7
Ryan Somma <ideonexus@gmail.com>
2015-07-10 15:04:42 -0400

    Synch with master.

---
+++ Book/js/mainmenu.js
@@ -0,0 +1,5 @@
+
+var headerHtml = '<div id="headLinks"><a href="index.html">Home</a> | <a href="01Introduction.html">01</a> | <a href="02ExplorableExplanations.html">02</a> | <a href="03HackerMindset.html">03</a> | <a href="04TheStack.html">04</a> | <a href="05CodingConcepts.html">05</a> | <a href="06OurEnvironment.html">06</a> | <a href="07CopyPasteProgramming.html">07</a> | <a href="08LetYourStudentsLead.html">08</a> | <a href="#">09</a> | <a href="10LearnTeachLead.html">10</a> | <a href="11Attributions.html">11</a></div>';
+$( document ).ready(function() {
+    $("#headLinks").html(headerHtml);
+});
